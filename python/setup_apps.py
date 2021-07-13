44bbdaf9c11cdb1f2a6ba37b680b5b50d745cd82
Lord Mike <lordmike@iki.fi>
2021-07-13 16:54:31 +0300

    Fix: venv path :)

--- python/setup_apps.py
+++ python/setup_apps.py
@@ -27,7 +27,7 @@
 #__docformat__ = 'reStructuredText'
 
 __license__ = "MIT License"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __revision__ = "setup_apps.py  v" + __version__ + " (2021-07-13)"
 
 import sys
@@ -62,8 +62,9 @@ def activate_virtual_environment():
     #py_home = 'C:/Users/lordmike/Envs/venv-LMAutoSetBotWin'
     #py_home = os.environ.get('USERPROFILE') + '/Envs/venv-LMAutoSetBotWin'
     user_home = setup_apps.util.home_path()
-    py_home = user_home + '/Envs/venv-LMAutoSetBotWin'
-    activate_this = py_home + '/Scripts/activate_this.py'
+    py_home = user_home + '/Envs/venv-LMAutoSetBotUbM'
+    #activate_this = py_home + '/Scripts/activate_this.py'
+    activate_this = py_home + '/bin/activate_this.py'
 
     with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
